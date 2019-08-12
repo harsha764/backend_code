42e62c9329c30a9ccabb155e36e4f7e7a4a6c77a
harsha764 <hreddy710@gmail.com>
2019-08-12 18:40:49 +0530

    latest changes

--- add_form.php
+++ add_form.php
@@ -6,8 +6,6 @@
 	if($data['apiname']= "addform"){
 		$title = $data["title"];
 		$unique_id = substr(str_shuffle(str_repeat('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', mt_rand(1,10))), 1, 10);
-
-
 		$sql = mysqli_query($conn,"INSERT INTO form_list(unique_id,name) VALUES('$unique_id','$title')");
 		
 		foreach ($data as $key => $value){
--- update_form.php
+++ update_form.php
@@ -3,7 +3,7 @@
 	$data = json_decode(file_get_contents("php://input"), TRUE);
 	$resobj = new stdClass;
 	
-	if($data['apiname']= "updateform" && $data["name"] !== ''){
+	if($data['apiname']= "updateform" && $data["id"] !== ''){
 		$unique_id =  $data["id"];
 		// Delete previous records
 		$sql = mysqli_query($conn,"DELETE FROM form_list WHERE unique_id = '$unique_id'");
